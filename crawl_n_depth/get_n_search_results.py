ba41f013416ea4384b525267bcee302ec9e167c3
Gihan Gamage <gihangamage2015@gmail.com>
2020-04-15 11:40:57 +0530

    Made more robust for captchas

--- crawl_n_depth/get_n_search_results.py
+++ crawl_n_depth/get_n_search_results.py
@@ -1,14 +1,53 @@
-from random import choice
-
 import requests
-
+from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from bs4.element import Tag
-import csv
 from fake_useragent import UserAgent
-import time
-from selenium.webdriver.firefox.options import Options
+from random import choice
+
+def proxy_generator():
+    response = requests.get("https://sslproxies.org/")
+    soup = BeautifulSoup(response.content, 'html5lib')
+    proxy = {'https': choice(list(map(lambda x:x[0]+':'+x[1], list(zip(map(lambda x:x.text,
+	   soup.findAll('td')[::8]), map(lambda x:x.text, soup.findAll('td')[1::8]))))))}
+    return proxy
+
+def use_chrome():
+    ua = UserAgent()
+    userAgent = ua.random #get a random user agent
+    options = webdriver.ChromeOptions()  # use headless version of chrome to avoid getting blocked
+    options.add_argument('headless')
+    options.add_argument(f'user-agent={userAgent}')
+    options.add_argument("start-maximized")# // open Browser in maximized mode
+    options.add_argument("disable-infobars")# // disabling infobars
+    options.add_argument("--disable-extensions")# // disabling extensions
+    options.add_argument("--disable-gpu")# // applicable to windows os only
+    options.add_argument("--disable-dev-shm-usage")# // overcome limited resource problems
+    browser = webdriver.Chrome(chrome_options=options,  # give the path to selenium executable
+                               # executable_path='F://Armitage_lead_generation_project//chromedriver.exe'
+                               executable_path='utilities/chromedriver.exe'
+                               )
+    return browser
+
+def use_firefox():
+    profile = webdriver.FirefoxProfile()
+    # profile.set_preference("browser.privatebrowsing.autostart", True)
+    proxies = ['54.213.66.208', "43.250.242.251", "192.248.15.153"
+               ]
+    prx = choice(proxies)
+    print(prx)
+    profile.set_preference("network.proxy.type", 1)
+    profile.set_preference("network.proxy.http", prx)
+    profile.set_preference("network.proxy.http_port", 80)
+    options = Options()
+    options.headless = True
+
+    profile.update_preferences()
+    browser = webdriver.Firefox(firefox_options=options, firefox_profile=profile,
+                                executable_path='utilities/geckodriver')
+    return browser
+
 def getGoogleLinksForSearchText(searchText,number_of_results):#given a search query get first n results from google
     """
 
@@ -17,76 +56,36 @@ def getGoogleLinksForSearchText(searchText,number_of_results):#given a search qu
     :return: save resulted links to csv along with title and description
     """
     print("searching on google",searchText)
-    # searchText=searchText+' -site:businessnews.com -site:facebook.com -site:bloomberg.com -site:asic.hkcorporationsearch.com -site:abnlookup.net -site:aubiz.net -site:auscompanies.com -site:google.com -site:whitepages.com.au -site:abn-lookup.com -site:au.mycompanydetails.com -site:infobel.com -site:truelocal.com.au -site:realestate.com.au -site:localsearch.com.au -site:linkedin.com -site:www.dnb.com -site:wikipedia.org'
 
-    proxies = ['54.213.66.208', "43.250.242.251", "192.248.15.153"]
-    options = webdriver.ChromeOptions()#use headless version of chrome to avoid getting blocked
-    options.add_argument('headless')
-    # options.add_argument('--proxy-server=%s' % proxies[0])
-
-    # options.add_argument("-user-data-dir=C:\Users\Gihan Gamage\AppData\Local\Google\Chrome\User Data\Default")
-    browser = webdriver.Chrome(chrome_options=options,#give the path to selenium executable
-            # executable_path='F://Armitage_lead_generation_project//chromedriver.exe'
-            executable_path = 'utilities//chromedriver.exe')
-     # initiate the class (you can pass an apikey if you have one)
-
-    # profile = webdriver.FirefoxProfile()
-    # profile.set_preference("browser.privatebrowsing.autostart", True)
-    # proxies = ['54.213.66.208', "43.250.242.251", "192.248.15.153"
-    #
-    #            ]
-    # prx = choice(proxies)
-    # print(prx)
-    # profile.set_preference("network.proxy.type", 1)
-    # profile.set_preference("network.proxy.http", prx)
-    # profile.set_preference("network.proxy.http_port", 80)
-    # options = Options()
-    # options.headless = True
-    #
-    # profile.update_preferences()
-    # browser = webdriver.Firefox(firefox_options=options, firefox_profile=profile,
-    #                             # executable_path='utilities//geckodriver.exe'
-    #                             executable_path = 'F://Armitage_project/crawl_n_depth/utilities/geckodriver.exe'
-    #                             )
-
-    # random_proxy_us = api.get_proxy(country="US")
     #buildingsearch query
-    searchGoogle = f"https://google.com/search?q={searchText}"+""+"&num=" + str(number_of_results)
-    # print(searchGoogle)
-    ua = UserAgent()
-    userAgent = ua.random
-    # print(userAgent)
-    # options.add_argument(f'user-agent={userAgent}')
+    searchGoogle = URL = f"https://google.com/search?q={searchText}"+"&num=" + str(number_of_results)
 
-    # proxies = {"http": "http://10.10.1.10:3128",
-    #            "https": "http://10.10.1.10:1080"}
 
-    # requests.get("http://example.org", proxies=proxies)
+    browser = use_chrome()#get a chrome instance
     browser.get(searchGoogle)
-    # print(browser.page_source)
-    headers = {'User-Agent': userAgent}
-    r = requests.get(searchGoogle, headers=headers)
     pageSource = browser.page_source
-    # print(pageSource)
+
     soup = BeautifulSoup(pageSource, 'html.parser')#bs4
     is_captcha_on_page = soup.find("div", id="recaptcha") is not None
-    count = 0
-    if(is_captcha_on_page):return 'captcha'
-    # while(is_captcha_on_page):
-    #     count=count+1
-    #     print("captch is detected "+str(count)+" times")
-    #     print("waiting more time",count*1200)
-    #     time.sleep(count*1200)
+
+    if(is_captcha_on_page):#a captcha triggered
+        return 'captcha'
+    # while (is_captcha_on_page):
+    #     count = count + 1
+    #     print("captch is detected " + str(count) + " times")
+    #     print("waiting more time", count * 1000)
+    #     time.sleep(count * 1000)
+    #     browser = use_chrome()
+    #
     #     browser.get(searchGoogle)
-    #     soup = BeautifulSoup(pageSource, 'html.parser')  # bs4
+    #     pageSource = browser.page_source
+    #     soup = BeautifulSoup(pageSource, 'html.parser')  #
+    #     # r = requests.get(searchGoogle, headers=headers)
+    #     # soup = BeautifulSoup(r.text, 'html.parser')
     #     is_captcha_on_page = soup.find("div", id="recaptcha") is not None
-
-
-
     results = []
     result_div = soup.find_all('div', attrs={'class': 'g'})
-    # print(result_div)
-    # print(len(result_div))
+
     for r in result_div:
         # Checks if each element is present, else, raise exception
         try:
@@ -119,8 +118,6 @@ def getGoogleLinksForSearchText(searchText,number_of_results):#given a search qu
             print(e)
             continue
 
-
-
     # with open('first_n_results.csv', mode='w', encoding='utf8') as results_file:#store search results in to a csv file
     #     results_writer = csv.writer(results_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     #
@@ -135,8 +132,8 @@ def getGoogleLinksForSearchText(searchText,number_of_results):#given a search qu
 # for searchResult in searchResults:
 #     print(searchResult)
 
-# #
-# searchResults = getGoogleLinksForSearchText("gampaha srilanka",3)
+#example
+# searchResults = getGoogleLinksForSearchText("Information System Australia",3)
 # for searchResult in searchResults:
 #     print(searchResult)
 
