015baf61bfd74658b63652e8ee3aca6437905f21
Gihan Gamage <gihangamage2015@gmail.com>
2020-06-08 08:20:31 +0530

    csv_export file updated

--- crawl_n_depth/Simplified_System/export_csv.py
+++ crawl_n_depth/Simplified_System/export_csv.py
@@ -9,7 +9,7 @@ import csv
 def refer_collection():
   myclient = pymongo.MongoClient("mongodb+srv://gatekeeper:oMBipAi6zLkme3e9@armitage-i0o8u.mongodb.net/test?retryWrites=true&w=majority")
   mydb = myclient["miner"]  # creates a database
-  mycol = mydb["simplified_dump"]  # creates a collection
+  mycol = mydb["simplified_dump_min"]  # creates a collection
   return mycol
 
 
@@ -25,21 +25,18 @@ def get_all_ids():
 def export_profiles(id_list):
     mycol = refer_collection()
     # store data in a csv file
-    dump_name = 'company_dump.csv'
+    dump_name = 'company_dump_min.csv'
     with open(dump_name, mode='w',encoding='utf8', newline='') as results_file:  # store search results in to a csv file
         results_writer = csv.writer(results_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-        attributes_a = ['_id','search_text','title', 'link','description','rich_description','comp_name','addresses', 'emails',
-                                'social_media_links','telephone_numbers','kpe_results',
-                                'dnb_cp_info','oc_cp_info','linkedin_cp_info','comp_type_pred','company_size_li',
-                                'description_li','founded_li','headquarters_li','image_li','industry_li',
-                                 'name_li','num_employees_li','specialties_li','type_li','website_li','business_number_oc',
-                                 'company_number_oc','company_type_oc','incorporation_date_oc','jurisdiction_oc',
-                                 'registered_address_adr_oc','registry_page_oc','status_oc','agent_name','agent_address',
-                                 'dissolution_date_oc', 'annual_return_last_made_up_date_oc', 'directors_or_officers_oc',
-                                 'company_trade_name_dnb', 'company_address_dnb', 'company_summary_dnb',
-                                 'company_web_dnb', 'company_tp_dnb', 'company_type_dnb',
-                                 'company_related_industries_dnb', 'company_snapshot_dnb', 'company_revenue_dnb','company_contacts_dnb' ]
+
+        attributes_a = ['_id','search_text','title','link'
+        ,'description','comp_name','email','keywords'
+        ,'founded_year','revenue'
+        ,'funding','No_of_employees','headquarters'
+        ,'telephone_number','contact_person'
+        ,'address','type_or_sector']
+
         results_writer.writerow(attributes_a)
         for i,entry_id in enumerate(id_list):
             print("dumping record",i)
@@ -48,12 +45,7 @@ def export_profiles(id_list):
             data_list = []
             for each_a in attributes_a:
                 try:
-                    if((each_a=='addresses') or (each_a=='emails') or (each_a=='social_media_links') or (each_a=='telephone_numbers')):
-                        data_list.append(data[0][each_a][:5])#truncating addresses
-                    elif((each_a=='wordcloud_results_tri')):
-                        data_list.append(data[0][each_a][:10])#trucating tokens
-                    else:
-                        data_list.append(data[0][each_a])#do not trunctate anything else
+                    data_list.append(data[0][each_a])#do not trunctate anything else
                 except KeyError:
                         data_list.append('None')#If data not there mark it as none
 
@@ -69,7 +61,7 @@ def export_all():
     print(all_ids)
     export_profiles(all_ids)
 
-
+export_all()
 
 #give ids in simplified_dump collection.
 # export_profiles([ObjectId('5eb44a5818e5aaac743c1e50'),ObjectId('5eb44c3386ed61b292cd7528')])
